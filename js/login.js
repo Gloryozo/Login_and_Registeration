3c05abc65e2dd8ac7d7d8d575542b5c238ee54a3
Gloryozo <gloryozoji@gmail.com>
2024-03-28 17:57:09 +0200

    made little addition changes to login.js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,5 @@
 const input = document.querySelector('input');
 const button = document.querySelector('button');
-const error = document.querySelector('.error');
 
 // Add an event listener to the input field
 input.addEventListener('keypress', (event) => {   
@@ -29,9 +28,21 @@ input.addEventListener('keypress', (event) => {
 
 // Add an event listener to the button  
 button.addEventListener('click', (event) => {
-        // Trigger the button click event
-        button.click()
+    // Prevent the default form submission
+    event.preventDefault();
 
-        // Prevent the default form submission
-        event.preventDefault()
-    });  
+    const usernameOrEmail = input.value.trim(); // Get the value of the input field
+    const password = input.value.trim(); // Get the value of the input field
+
+    // Check if the input values are not empty
+    if (usernameOrEmail === '' || password === '') {
+        alert('Please enter both username/email and password');
+        return;
+    }
+     // Log the values to the console
+     console.log('Username/Email:', usernameOrEmail);
+     console.log('Password:', password);
+ 
+     // Clear the input fields after submission (optional)
+     input.value = '';
+ });
